044ef782b8d1667686e6c82d9bcb13e6fa0cb9db
emre-aksu <emreaksu92@outlook.com>
2025-03-14 16:09:36 +0300

    Infrastructure layer added. Data access operations (retrieving by ID, listing, filtering, inserting, updating, deleting) have been structured to be managed centrally in this layer.

---
+++ NewsPaperRestAPI/InfrastructorLayer/DataAccess/Contracts/IStorageRepository.cs
@@ -0,0 +1,18 @@
+﻿using InfrastructorLayer.Model;
+using System.Linq.Expressions;
+
+namespace InfrastructorLayer.DataAccess.Contracts
+{
+    public interface  IStorageRepository<TEntity, TId>
+        where TEntity:BaseRecord<TId>
+    {
+        Task<List<TEntity>> GetAllAsync(params string[] includeList);
+        Task<List<TEntity>> FilterAsync(Expression<Func<TEntity,bool>> predicate,params string[] includeList);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params string[] includeList);
+        Task<TEntity> GetByIdAsync(TId id,params string[] includeList);
+        Task InsertAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task DeleteAsync(TId id);
+    }
+}
---
+++ NewsPaperRestAPI/InfrastructorLayer/DataAccess/Implementations/StorageRepository.cs
@@ -0,0 +1,99 @@
+﻿using InfrastructorLayer.DataAccess.Contracts;
+using InfrastructorLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace InfrastructorLayer.DataAccess.Implementations
+{
+    public abstract class StorageRepository<TEntity, TId, TContext> : IStorageRepository<TEntity, TId>
+        where TEntity : BaseRecord<TId>
+        where TContext : DbContext, new()
+    {
+        public async Task DeleteAsync(TEntity entity)
+        {
+            using TContext ctx = new();
+            ctx.Set<TEntity>().Remove(entity);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TId id)
+        {
+            var entity = await GetByIdAsync(id);
+            using TContext ctx = new();
+            ctx.Set<TEntity>().Remove(entity);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task<List<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> predicate, params string[] includeList)
+        {
+            using TContext ctx = new();
+            IQueryable<TEntity> query = ctx.Set<TEntity>();
+            if (includeList != null && includeList.Length > 0)
+            {
+                foreach (var include in includeList)
+                {
+                    query = query.Include(include);
+                }
+            }
+            query = query.Where(predicate);
+            return await query.ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(params string[] includeList)
+        {
+            using TContext ctx = new();
+            IQueryable<TEntity> query = ctx.Set<TEntity>();
+            if (includeList != null && includeList.Length > 0)
+            {
+                foreach (var include in includeList)
+
+                    query = query.Include(include);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params string[] includeList)
+        {
+            using TContext ctx = new();
+            IQueryable<TEntity> query = ctx.Set<TEntity>();
+            if (includeList != null)
+            {
+                foreach (var item in includeList)
+                {
+                    query = query.Include(item);
+                }
+            }
+            return await query.SingleOrDefaultAsync(predicate);
+        }
+
+        public async Task<TEntity> GetByIdAsync(TId id, params string[] includeList)
+        {
+            using TContext ctx = new();
+            IQueryable<TEntity> query = ctx.Set<TEntity>();
+            if (includeList != null)
+            {
+                foreach (var item in includeList)
+
+                    query = query.Include(item);
+
+            }
+            return await query.SingleOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            using (TContext ctx = new())
+            {
+                await ctx.Set<TEntity>().AddAsync(entity);
+                await ctx.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            using TContext ctx = new();
+            ctx.Set<TEntity>().Update(entity);
+            await ctx.SaveChangesAsync();
+        }
+    }
+}
